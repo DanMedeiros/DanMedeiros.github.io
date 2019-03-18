bf2a04cb9a747067c32bd10c918c318cd9520560
Danilo Medeiros <web.medeiros@gmail.om>
2019-03-18 14:50:17 -0300

    Meu portfólio inicial

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let progressoHtml = document.querySelector('#html');
+let progressoJavaScript = document.querySelector('#javaScript');
+let progressoNode = document.querySelector('#node');
+let progressoReact = document.querySelector('#react')
+let progressoAngular = document.querySelector('#angular');
+let nome = document.querySelector('#nome');
+
+progressoHtml.style.width = '90%';
+progressoJavaScript.style.width = '50%';
+progressoNode.style.width = '20%';
+progressoReact.style.width = '20%';
+progressoAngular.style.width = '20%';
